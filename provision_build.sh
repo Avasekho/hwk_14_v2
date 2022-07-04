473bb670a87776a3c69969191c6ffdd1e3d71441
Anton <gish2008@gmail.com>
2022-07-04 14:47:21 +0300

    userdata change

--- provision_build.sh
+++ provision_build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt install -y maven default-jdk awscli
-mkdir -p /home/ubuntu/.ssh/
-aws s3 cp s3://avasekho.test.credentials/id_rsa /home/ubuntu/id_rsa
-aws s3 cp s3://avasekho.test.credentials/config /home/ubuntu/config
-mv /home/ubuntu/id_rsa /home/ubuntu/.ssh/id_rsa
-chmod 400 /home/ubuntu/.ssh/id_rsa
-mv /home/ubuntu/config /home/ubuntu/.ssh/config
-chmod 600 /home/ubuntu/.ssh/config
+mkdir -p ~/.ssh/
+aws s3 cp s3://avasekho.test.credentials/id_rsa /tmp/id_rsa
+aws s3 cp s3://avasekho.test.credentials/config /tmp/config
+mv /tmp/id_rsa ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+mv /tmp/config ~/.ssh/config
+chmod 600 ~/.ssh/config
 git config --global core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 git clone ssh://APKAVNWETNK3NSW6CY4P@git-codecommit.us-east-1.amazonaws.com/v1/repos/boxfuze /tmp/boxfuze
 cd /tmp/boxfuze/
