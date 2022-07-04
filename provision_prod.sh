224594cf4d90c3f5faaf8695e7b12d439bf0e1f6
Anton <gish2008@gmail.com>
2022-07-04 23:15:25 +0300

    sh removed

--- provision_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install -y tomcat9 awscli
-sudo chmod 777 /var/lib/tomcat9/webapps/
-aws s3 cp s3://boxfuze.avasekho.test/hello-1.0.war /var/lib/tomcat9/webapps/hello-1.0.war
